e3e340865c80a3ae9675f6d89160bf26680140bd
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-01-04 03:03:50 +0200

    Ready

--- HomeWorkLesson2.1/ViewController.swift
+++ HomeWorkLesson2.1/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
+enum CurrentColor {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentColor = CurrentColor.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        startButton.layer.cornerRadius = 15
+        
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
     }
 
+    override func viewWillLayoutSubviews() {
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = redColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = redColorView.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
 
+        switch currentColor {
+        case .red:
+            redColorView.alpha = lightIsOn
+            greenColorView.alpha = lightIsOff
+            currentColor = .yellow
+        case .yellow:
+            yellowColorView.alpha = lightIsOn
+            redColorView.alpha = lightIsOff
+            currentColor = .green
+        case .green:
+            greenColorView.alpha = lightIsOn
+            yellowColorView.alpha = lightIsOff
+            currentColor = .red
+        }
+    }
 }
 
